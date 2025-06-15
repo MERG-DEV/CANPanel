ab2eec2787c90381251a932f3be01f8d0cd104e9
pnbgit <16050680+pnbgit@users.noreply.github.com>
2025-06-15 18:15:44 +0100

    Conditional button types for KMRS station panel

--- panelFLiM.c
+++ panelFLiM.c
@@ -87,11 +87,11 @@ const rom char          module_type[] = MODULE_TYPE;
 
 #if defined KMRSSTN
 
-const rom BYTE nvTable[NV_NUM] =       { 0,0,16,0,8,0,8,8,10,0,0,0,0,0,0,0,
+const rom BYTE nvTable[NV_NUM] =       { 0,0,32,0,8,0,8,8,10,0,0,0,0,0,0,0  // NV no.
                                         DPB,DPB,DPB,DPB,DPB,DPB,DPB,DPB,    // 17-24
                                         DPB,DPB,DPB,DPB,DPB,0x1b,DPB,DPB,    // 18-32
                                         DPB,DPB,DPB,DPB,DPB,0x1b,DPB,DPB,    // 33-40
-                                        DPB,DPB,DPB,DPB,DPB,0x1b,DPB,DPB,    // 41-48
+                                        DPB,DPB,DPB,DPB,DPB,0x1b,DPB,0x1b,    // 41-48
                                         0x1b,DPB,DPB,DPB,DPB,DPB,0x1b,DPB,    // 49-56
                                         DPB,0x1b,DPB,DPB,DPB,DPB,0x1b,DPB,   // 57-64
                                         DPB,0x1b,DPB,DPB,DPB,DPB,DPB,0x1b,    // 65-72
